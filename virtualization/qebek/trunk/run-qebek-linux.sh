4b53b28dfbbe4272193ec7db8df0a298bdac8949
Wasif <wmalik@gmail.com>
2011-05-17 00:02:51 +0100

    README added

--- virtualization/qebek/trunk/run-qebek-linux.sh
+++ virtualization/qebek/trunk/run-qebek-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-IMAGE_FILE=linux-0.2.img
+IMAGE_FILE=../images/linux-0.2.img
 
-i386-softmmu/qebek -localtime -m 1024 ../images/linux-0.2.img 2>&1 | python parser.py
+i386-softmmu/qebek -localtime -m 1024 $IMAGE_FILE 2>&1 | python parser.py
 
