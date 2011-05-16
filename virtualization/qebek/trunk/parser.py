1ce203dad062f71b894b550ca53097d2673aba44
Marcus Ljungblad <marcus@ljungblad.nu>
2011-05-16 16:33:35 +0100

    added functions for reasoning about qebek output

--- virtualization/qebek/trunk/parser.py
+++ virtualization/qebek/trunk/parser.py
@@ -1,6 +1,39 @@
 import sys
 from time import strftime
 
+# Defines sets of files which are flagged
+NOACCESS = set()
+HIGH = set()
+MEDIUM = set()
+LOW = set()
+
+
+def is_illegal_access(afile): 
+    if afile in NOACCESS:
+        log_illegal(afile)
+
+def is_cp_ok(fromfile, tofile=None):
+    if not tofile is None:
+        check_high_to_low_copy(fromfile, tofile)
+        return
+
+    if fromfile in HIGH:
+        log_copy(fromfile + " High Priv file")
+        return
+
+def check_high_to_low_copy(fromfile, tofile):
+    if fromfile in HIGH and (tofile in MEDIUM or tofile in LOW):
+        log_copy("High Priv file: " + fromfile + " copied to: " + tofile)
+    elif fromfile in MEDIUM and tofile in LOW:
+        log_copy("Medium Priv file: " + fromfile + " copied to: " + tofile)
+
+def log_illegal(comment):
+    log(comment, "Illegal_access")
+def log_copy(comment):
+    log(comment, "Illegal_copy")
+def log(comment, logtype): 
+    print "[" + str(datetime.datetime.now()) + "|" + logtype + "] " + comment
+
 def getTime():
 	return strftime("%Y-%m-%d %H:%M:%S")
 
